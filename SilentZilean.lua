fa86b77674f4f41ccad4c45274f6db90cb62474f
bolsilent <uareapinny@yahoo.com>
2016-02-29 07:15:25 -0800

    Silent Zilean
    Old version was for old zilean
    Decreased fps drops

--- SilentZilean.lua
+++ SilentZilean.lua
@@ -1,122 +1,160 @@
---[[ Zilean by tivia ]]--
+
+
+require "UPL"
+UPL = UPL()
 
 if myHero.charName ~= "Zilean" then return end
 
 
-local QRange = 700
-local ERange = 700
-local RRange = 900
-local QREADY, WREADY, EREADY, RREADY = false, false, false, false 
 
+local enemyChamps, allyChamps = GetEnemyHeroes(), GetAllyHeroes()
+local enemyCount
+local allyCount
 
 
 function OnLoad()
 
-	sZileanConfig = scriptConfig("sZilean - Main","sZilean")
-	sZileanConfig:addSubMenu("Basic Settings", "Basic")
-	sZileanConfigUltimate = scriptConfig("sZilean - Ultimate", "sZileanUlt")
-	sZileanConfig:addSubMenu("Draw Settings", "Draw")
-	sZileanConfig.Basic:addParam("supportmode", "Support Mode", SCRIPT_PARAM_ONOFF, false)
-	sZileanConfig.Basic:addParam("combo","QWQ Combo", SCRIPT_PARAM_ONKEYDOWN, false, 32)
-	sZileanConfig.Basic:addParam("harass", "Q Harass", SCRIPT_PARAM_ONOFF, true, 67)
+ enemyCount = #enemyChamps
+    allyCount = #allyChamps
+    
 	
-	sZileanConfigUltimate:addParam("autoUlt", "Auto Ultimate", SCRIPT_PARAM_ONOFF, true)
-	sZileanConfig.Draw:addParam("drawQ", "Draw Q Range", SCRIPT_PARAM_ONOFF, true)
-	sZileanConfig.Draw:addParam("drawW", "Draw W Range", SCRIPT_PARAM_ONOFF, true)
-	sZileanConfig.Draw:addParam("drawE", "Draw E Range", SCRIPT_PARAM_ONOFF, true)
-	ts = TargetSelector(TARGET_LESS_CAST_PRIORITY, RRange, DAMAGE_PHSYCAL, true)
-	sZileanConfig:addTS(ts)
+	skillQ =
+    {
+	
+        range = 900,
+				delay = .5,
+				radius = 150,
+				speed = math.huge,
+        dmg = function() return (20 + (GetSpellData(0).level*55) + (myHero.ap*0.90)) end
+				
+    }
+	
+	skillW =
+    {
+        range =  1200
+    }
+	
+	skillE =
+    {
+        range = 550
+    
+    }
+	
+	skillR =
+    {
+        range = 900
+     
+    }
+	
+	UPL:AddSpell(_Q, { speed = skillQ.speed, delay = skillQ.delay, range = skillQ.range, width = skillQ.radius, collision = false, aoe = true, type = "circular" })
+	
+
+	Config = scriptConfig("Silent Zil", "workdammit")
+	Config:addSubMenu("Basic Settings", "Basic")
+
+	Config.Basic:addParam("supportmode", "supportmode", SCRIPT_PARAM_ONOFF, false)
+	Config.Basic:addParam("harass", "harass", SCRIPT_PARAM_ONOFF, true, 67)
+
+	Config.Basic:addParam("combo","combo", SCRIPT_PARAM_ONKEYDOWN, false, 32)
+		UPL:AddToMenu(Config)
+	Config:addParam("hc", "Accuracy:", SCRIPT_PARAM_SLICE, 2,0,3,1)
+	Config:addSubMenu("Orbwalker Settings", "Orbwalking")
+	
+		Config:addSubMenu("Ult", "Ult")
+
+	Config.Ult:addParam("autoUlt", "autoUlt", SCRIPT_PARAM_ONOFF, true)
+	
+	Config:addSubMenu("Draw", "Draw")
+	Config.Draw:addParam("drawQ", "Draw Q Range", SCRIPT_PARAM_ONOFF, true)
+	Config.Draw:addParam("drawW", "Draw W Range", SCRIPT_PARAM_ONOFF, true)
+	Config.Draw:addParam("drawE", "Draw E Range", SCRIPT_PARAM_ONOFF, true)
 
 
-	player = GetMyHero()
-	PrintChat(" >> Silent Zilean V2 Loaded")
+			ts = TargetSelector(TARGET_LESS_CAST_PRIORITY, skillW.range, DAMAGE_MAGIC, true)
+		Config:addTS(ts)
 
 
-	for _, ally in ipairs(GetAllyHeroes()) do
-		sZileanConfigUltimate:addParam(ally.charName, ally.charName, SCRIPT_PARAM_ONOFF, true)
+
+	
+	for i = 1, allyCount do
+    local ally = allyChamps[i]
+	Config.Ult:addParam(ally.charName, ally.charName, SCRIPT_PARAM_ONOFF, true)
 	end
+	
+
+DelayAction(CheckOrbwalk, 8)
+	end
+	
+function Qcast()
+    if not QREADY then return end
+        for i = 1, enemyCount do
+            local enemy = enemyChamps[i]
+            if ValidTarget(enemy, skillQ.range) then
+			CastPosition, HitChance, HeroPosition = UPL:Predict(_Q, myHero, enemy)
+				if HitChance >= Config.hc then
+					CastSpell(_Q, CastPosition.x, CastPosition.z)
+				end
+            end
+        end
+end
+
+ function CheckOrbwalk()
+	 if _G.Reborn_Loaded and not _G.Reborn_Initialised then
+        DelayAction(CheckOrbwalk, 1)
+    elseif _G.Reborn_Initialised then
+        sacused = true
+		Config.Orbwalking:addParam("info11","SAC Detected", SCRIPT_PARAM_INFO, "")
+    elseif _G.MMA_Loaded then
+		Config.Orbwalking:addParam("info11","MMA Detected", SCRIPT_PARAM_INFO, "")
+		mmaused = true
+	else
+		require "SxOrbWalk"
+		SxOrb:LoadToMenu(Config.Orbwalking, false) 
+		sxorbused = true
+		SxOrb:RegisterAfterAttackCallback(MyAfterAttack)
+		DelayAction(function()		
+			if SxOrb.Version < 2.3 then
+				Print("Your SxOrbWalk library is outdated, please get the latest version!")
+			end
+		end, 5)
+	end
+end
+
+function Wcast()
+if not EREADY then return end
+CastSpell(_W)
+end
+
+
+function Ecast()
+if not EREADY then return end
+	for i = 1, allyCount do
+    local ally = allyChamps[i]
+	if GetDistance(ally) < ERange then
+					CastSpell(_E, ally)
+				end
+				end
+end
+
+function Ecast2()
+if not EREADY then return end
+        for i = 1, enemyCount do
+            local enemy = enemyChamps[i]
+            if ValidTarget(enemy, skillE.range) then
+				CastSpell(_E, enemy)
+            end
+        end
 
 end
 
-function OnTick()
-Checks()
-	if ts.target then
-			if sZileanConfig.Basic.combo then 
-				combo() 
-			end
-			if sZileanConfig.Basic.harass then
-				harass()
-			end
-			if sZileanConfigUltimate.autoUlt then
-				autoUlt()
-			end
-			if sZileanConfig.Basic.supportmode then
-				supportmode()
-			end
-	end
-end
 
 function Checks()
 QREADY = ((myHero:CanUseSpell(_Q) == READY) or (myHero:GetSpellData(_Q).level > 0 and myHero:GetSpellData(_Q).currentCd <= 0.4)) WREADY = ((myHero:CanUseSpell(_W) == READY) or (myHero:GetSpellData(_W).level > 0 and myHero:GetSpellData(_W).currentCd <= 0.4))
 EREADY = ((myHero:CanUseSpell(_E) == READY) or (myHero:GetSpellData(_E).level > 0 and myHero:GetSpellData(_E).currentCd <= 0.4))
 RREADY = ((myHero:CanUseSpell(_R) == READY) or (myHero:GetSpellData(_R).level > 0 and myHero:GetSpellData(_R).currentCd <= 0.4))
 IREADY = (ignite ~= nil and myHero:CanUseSpell(ignite) == READY)
-ts:update()
-end
+  
 
-function castq()
-	for i = 1, enemyCount do
-    local enemy = enemyChamps[i]
-        if ValidTarget(enemy, QRange) then
-			if QREADY then 
-				CastSpell(_Q, enemy.x, enemy.z)
-			end
-		end
-	end
-end
-
-function caste()
- for k, ally in pairs(GetAllyHeroes()) do
-				if GetDistance(ally) < ERange then
-					CastSpell(_E, ally)
-				end
-	end
-end
-
-function caste2()
-    for i = 1, enemyCount do
-    local enemy = enemyChamps[i]
-        if ValidTarget(enemy, ERange) then
-			if EREADY then 
-				CastSpell(_E, enemy)
-			end
-		end
-	end
-end
-
-function supportmode(target)
-Checks()
-castq()
-caste()
-	end
-
-
-
-		
-
-function combo()
-Checks()
-caste2()
-castq()
-if WREADY and (myHero:CanUseSpell(_Q) == COOLDOWN) then CastSpell(_W) end
-castq()
-	end
-
-
-		
-function harass()
-Checks()
-castq()
 end
 
 
@@ -134,11 +172,12 @@ end
 function autoUlt()
 	if not myHero:CanUseSpell(_R) == READY then return end
 	local ultTarget
-	if myHero.health / myHero.maxHealth < 0.4 and CountEnemies(1000, myHero) > 0 then
+	if myHero.health / myHero.maxHealth < 0.4 and CountEnemies(700, myHero) > 0 then
 		ultTarget = myHero
 	end
-	for _, ally in ipairs(GetAllyHeroes()) do
-		if sZileanConfigUltimate[ally.charName] and ally.health / ally.maxHealth < 0.3 and GetDistance(ally) <= RRange and CountEnemies(1000, ally) > 0 then
+	for i = 1, allyCount do
+    local ally = allyChamps[i]
+		if Config.Ult[ally.charName] and ally.health / ally.maxHealth < 0.4 and GetDistance(ally) <= skillR.range and CountEnemies(700, ally) > 0 then
 			if not ultTarget or ultTarget.health > ally.health then
 				ultTarget = ally
 			end
@@ -149,9 +188,48 @@ end
 
 function OnDraw()
 	if myHero.dead then return end
-	if QREADY and sZileanConfig.Draw.drawQ then DrawCircle(myHero.x, myHero.y, myHero.z, QRange, 0x80408000) end
-	if EREADY and sZileanConfig.Draw.drawE then DrawCircle(myHero.x, myHero.y, myHero.z, ERange, 0x80408000) end
-	if RREADY and sZileanConfig.Draw.drawR then DrawCircle(myHero.x, myHero.y, myHero.z, RRange, 0x80408000) end
+	if QREADY and Config.Draw.drawQ then DrawCircle(myHero.x, myHero.y, myHero.z, 900, 0x80408000) end
+	if EREADY and Config.Draw.drawE then DrawCircle(myHero.x, myHero.y, myHero.z, 550, 0x80408000) end
+	if RREADY and Config.Draw.drawR then DrawCircle(myHero.x, myHero.y, myHero.z, 900, 0x80408000) end
 	end
 	
-	
+function supportmode(target)
+Checks()
+Qcast()
+Ecast()
+	end
+
+
+function OnTick()
+Checks()
+
+			if Config.Basic.combo then 
+				combo() 
+			end
+			if Config.Basic.harass then
+				harass()
+			end
+			if Config.Ult.autoUlt then
+				autoUlt()
+			end
+			if Config.Basic.supportmode then
+				supportmode()
+			end
+
+end
+		
+
+function combo()
+Checks()
+Ecast2()
+Qcast()
+if WREADY and (myHero:CanUseSpell(_Q) == COOLDOWN) then CastSpell(_W) end
+Qcast()
+	end
+
+
+		
+function harass()
+Checks()
+Qcast()
+end
